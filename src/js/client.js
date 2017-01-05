57d3fbffe186df833a4785186d6bdddaa762b03d
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-01-05 21:44:47 +0900

    make minimum react componenty

--- src/js/client.js
+++ src/js/client.js
@@ -1,1 +1,14 @@
-console.log("hoge");
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <h1>It works!</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
